0b97901deef70629ead2a2b55d8c06ccd4697335
orlion <orlion@foxmail.com>
2022-04-07 23:04:57 +0800

    init

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,8 +1,26 @@
 package main
 
-import "github.com/Orlion/lakeman/internal/server"
+import (
+	"log"
+	"time"
+
+	"github.com/Orlion/lakeman/internal/server"
+	dserver "github.com/dolthub/go-mysql-server/server"
+)
 
 func main() {
-	server := server.NewServer()
+	server, err := server.NewServer(dserver.Config{
+		Protocol:               "tcp",
+		Address:                "127.0.0.1:3306",
+		Version:                "5.7.1",
+		ConnReadTimeout:        5 * time.Second,
+		ConnWriteTimeout:       5 * time.Second,
+		MaxConnections:         5,
+		TLSConfig:              nil,
+		RequireSecureTransport: false,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
 	server.Start()
 }
